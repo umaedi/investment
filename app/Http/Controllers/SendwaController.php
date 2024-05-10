28c53acdb749008e6cca112240bb16aea9b1c6d7
umaedi <humaedi.kh.99@gmail.com>
2024-05-10 20:19:36 +0700

    devkh

--- app/Http/Controllers/SendwaController.php
+++ app/Http/Controllers/SendwaController.php
@@ -16,29 +16,67 @@ class SendwaController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $token = Cache::get('token');
-        if(!$token) {
-            $response = Http::post('https://apiks.ristekmuslim.com/public/v1/client/login', [
-                "uid" => "628170031000",
-                "pass" => "JiNgvO"
-            ]);
-            $responseData = $response->json();
+        // '6282128600231', 'bang Mahen 6282128600231', 'pak Figra 628970844853, bang Radius 62811222692'
+        $instanceID = '66069f95c302c5956ac9ca24';
+        $phoneNumbers = ['6285741492045', '6282128600231','628970844853', '62811222692'];
 
-            if($response->successful()) {
-                $token = $responseData['data']['token'];
-                Cache::put('token', $token, 172800);
-                // return $this->success('OK', 'Add Fund Investment Succes!');
-            }else {
-                return $this->error('Internal Server Error!');
-            }
-        }
+        $token = Cache::get('token');
+
+        $nomorWhatsAppBaru = "62" . substr($request->phone, 1);
+        $messagePersonal = '
+*Hi '. $request->NAME .'*
+
+Permintanaan penambahan dana sudah kami terima, PIC funding kami segera menghubungi bapak / ibu';
+        $response = Http::withToken($token)
+                    ->post('https://apiks.ristekmuslim.com/client/v1/message/send-text', [
+                        'instanceID'    => $instanceID,
+                        'phone'         => $nomorWhatsAppBaru,
+                        'message'       => $messagePersonal,
+                        'serverSend'    => 'false'
+                    ]);
+
+                    $responseData = $response->json();
+                    if($response->successful()) {
+                        if($responseData['status'] === 'error') {
+                            $response = Http::post('https://apiks.ristekmuslim.com/public/v1/client/login', [
+                                "uid" => "628170031000",
+                                "pass" => "JiNgvO"
+                            ]);
+
+                            $token = $responseData['data']['token'];
+                            Cache::put('token', $token, 172800);
+
+                            Http::withToken($token)->post('https://apiks.ristekmuslim.com/client/v1/message/send-text', [
+                                'instanceID'    => $instanceID,
+                                'phone'         => $nomorWhatsAppBaru,
+                                'message'       => $messagePersonal,
+                                'serverSend'    => 'false'
+                            ]);
+                        }
+                    }else {
+                        $response = Http::post('https://apiks.ristekmuslim.com/public/v1/client/login', [
+                            "uid" => "628170031000",
+                            "pass" => "JiNgvO"
+                        ]);
+
+                        $responseData = $response->json();
+
+                        if($response->successful()) {
+                            $token = $responseData['data']['token'];
+                            Cache::put('token', $token, 172800);
+                        }
+
+                        Http::withToken($token)->post('https://apiks.ristekmuslim.com/client/v1/message/send-text', [
+                            'instanceID'    => $instanceID,
+                            'phone'         => $nomorWhatsAppBaru,
+                            'message'       => $messagePersonal,
+                            'serverSend'    => 'false'
+                        ]);
+                    }
 
         {
-            // '6282128600231', 'bang Mahen 6282128600231', 'pak Figra 628970844853, bang Radius 62811222692'
-            $token = $token;
-            $instanceID = '66069f95c302c5956ac9ca24';
-            $phoneNumbers = ['6285741492045', '6282128600231','628970844853', '62811222692'];
-            $message = '
+            
+$message = '
 *Halo Bapak/Ibu '. $request->NAME .'*
 
 Terima kasih telah melakukan permintaan penambahan dana investasi kepada PT Rasa Aksata Nusantara dengan rincian sebagai berikut:
@@ -60,46 +98,7 @@ Jika ada pertanyaan lebih lanjut, silakan menghubungi di:
 *Email:* hello@duluin.com
 *No. Telp:* 08170031000
 
-Terima kasih.
-            ';
-
-            $nomorWhatsAppBaru = "62" . substr($request->phone, 1);
-            $messagePersonal = '
-*Hi '. $request->NAME .'*
-
-Permintanaan penambahan dana sudah kami terima, PIC funding kami segera menghubungi bapak / ibu';
-            $response = Http::withToken($token)
-                        ->post('https://apiks.ristekmuslim.com/client/v1/message/send-text', [
-                            'instanceID'    => $instanceID,
-                            'phone'         => $nomorWhatsAppBaru,
-                            'message'       => $messagePersonal,
-                            'serverSend'    => 'false'
-                        ]);
-
-                        $responseData = $response->json();
-                        if($response->successful()) {
-                            // Periksa apakah status respons adalah 'success'
-                            if($responseData['status'] === 'success') {
-                                $token = $responseData['data']['token'];
-                                // Simpan token dalam cache untuk 2 hari (172800 detik)
-                                Cache::put('token', $token, 172800);
-                                // Gunakan token untuk operasi selanjutnya di sini
-                            } else {
-                                // Respons tidak berhasil karena status bukan 'success'
-                                $response = Http::post('https://apiks.ristekmuslim.com/public/v1/client/login', [
-                                    "uid" => "628170031000",
-                                    "pass" => "JiNgvO"
-                                ]);
-                                $responseData = $response->json();
-                    
-                                    $token = $responseData['data']['token'];
-                                    Cache::put('token', $token, 172800);
-                            }
-                        } else {
-                            return $this->error("Internal Server Error!");
-                        }
-
-
+Terima kasih.';
             foreach ($phoneNumbers as $phoneNumber) {
                 $response = Http::withToken($token)
                 ->post('https://apiks.ristekmuslim.com/client/v1/message/send-text', [
